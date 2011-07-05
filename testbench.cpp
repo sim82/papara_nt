f6e0abb3ba03e8fa0e32e83e52628512caec33ec
Simon Berger <simerger@gmail.com>
2011-07-05 17:43:12 +0200

    added pars align testbench (and missing header...).

---
+++ testbench.cpp
@@ -0,0 +1,349 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <vector>
+#include <stdint.h>
+#include <climits>
+#include <algorithm>
+#include <utility>
+#include <map>
+
+#include <sys/time.h>
+
+#include "align_pvec_vec.h"
+
+static double gettime(void )
+{
+    struct timeval ttime;
+    gettimeofday(&ttime , 0);
+    return ttime.tv_sec + ttime.tv_usec * 0.000001;
+}
+
+
+class timer {
+    double m_st;
+    
+public:
+    timer() : m_st(gettime()) {};
+    
+    double elapsed() const {
+        return gettime() - m_st;
+    }
+    
+    
+};
+
+
+
+typedef int score_t;
+
+
+struct align_arrays {
+    std::vector<score_t> s;
+    std::vector<score_t> si;
+};
+
+
+static score_t align_pvec_score_seq( std::vector<int> &a, std::vector<unsigned int> &a_aux, const std::vector<uint8_t> &b, score_t mismatch_score, score_t match_cgap, score_t gap_open, score_t gap_extend, align_arrays &arr ) {
+
+    if( arr.s.size() < a.size()  ) {
+        arr.s.resize( a.size() );
+        arr.si.resize( a.size() );
+    }
+    
+    const score_t aux_cgap = 0x1;
+    
+    const size_t band_width = a.size() - b.size();
+    std::fill( arr.s.begin(), arr.s.end(), 0 );
+    
+    const score_t LARGE = 32000;
+//     std::fill( arr.si.begin(), arr.si.end(), LARGE );
+    arr.s.at(band_width+1) = LARGE;
+
+//     score_t min_score = LARGE;
+    
+    for( size_t ib = 0; ib < b.size(); ib++ ) {
+        int bc = b[ib];
+        
+        score_t last_sl = LARGE;
+        score_t last_sc = LARGE;
+        score_t last_sdiag = 0.0;
+        
+        size_t astart = ib;
+                
+        score_t * __restrict s_iter = &arr.s[0];
+
+        
+        bool lastrow = ib == (b.size() - 1);
+        
+       
+        last_sdiag = *s_iter;
+        
+        
+        for( size_t ia = astart; ia <= ib + band_width; ++ia, ++s_iter ) {  
+            score_t ac = a[ia];
+            const bool cgap = a_aux[ia] == aux_cgap;
+            
+            // determine match or mis-match according to parsimony bits coming from the tree.
+            bool is_match = ( ac & bc ) != 0;
+
+            score_t sm = last_sdiag;
+            
+            if( !is_match ) {
+                sm += mismatch_score;
+            }
+            
+            last_sdiag = *(s_iter+1);
+
+            score_t last_sc_OPEN; 
+            score_t sl_score_stay;
+            
+            if( cgap ) {
+                last_sc_OPEN = last_sc; 
+                sl_score_stay = last_sl;
+                sm += match_cgap;
+            } else {
+                last_sc_OPEN = last_sc + gap_open + gap_extend;  
+                sl_score_stay = last_sl + gap_extend;
+            }
+            
+            score_t sl = std::min( sl_score_stay, last_sc_OPEN );
+                        
+            last_sl = sl;
+            
+
+            score_t su = last_sdiag + mismatch_score;
+            
+//             std::cout << "x: " << sm << " " << su << " " << sl << "  ";
+            score_t sc = std::min( sm, std::min( su, sl ) );
+            
+            last_sc = sc;
+            *s_iter = sc;
+            
+        
+//             if( lastrow ) {
+//                 if( sc < min_score ) {
+//                     
+//                     min_score = sc;
+//                 }
+//             }
+           
+        }
+        
+        
+        
+        
+//         std::cout << "\n";
+//         getchar();
+    }
+    
+    return *std::min_element( arr.s.begin(), arr.s.begin() + band_width + 1 );
+    //std::cout << "min2: " << min_score << " " << min2 << "\n";
+
+    //return min_score;
+    
+}
+
+
+
+
+class testbench {
+    const static int score_gap_open = 1;
+    const static int score_gap_extend = 1;
+    const static int score_mismatch = 1;
+    const static int score_match_cgap = 4;
+    
+    std::vector<std::vector <int> > m_ref_pvecs;
+    std::vector<std::vector <unsigned int> > m_ref_aux;
+    
+    std::vector<std::vector <uint8_t> > m_qs_pvecs;
+
+    std::map<std::pair<size_t,size_t>,int> m_seq_res;
+    std::map<std::pair<size_t,size_t>,int> m_vec_res;
+    
+    
+    void load_qs( std::istream &is ) {
+        size_t num_qs;
+        is >> num_qs;
+        
+        m_qs_pvecs.resize( num_qs );
+        
+        for( int i = 0; i < m_qs_pvecs.size(); ++i ) {
+            size_t len;
+            is >> len;
+            while( isspace( is.get() ) && !is.eof() ) {}
+            is.unget();
+            
+            if( is.eof() ) {
+                throw std::runtime_error( "unexpected end of file" );
+            }
+            
+            m_qs_pvecs[i].resize(len);
+            is.read( (char*)&m_qs_pvecs[i][0], len );
+//             std::cout << "qs: " << i << " " << m_qs_pvecs[i].size() << "\n";
+        }
+    }
+    
+    void load_ref( std::ifstream &is ) {
+        size_t num_ref;
+        is >> num_ref;
+        m_ref_pvecs.resize( num_ref );
+        m_ref_aux.resize( num_ref );
+        
+        for( int i = 0; i < m_ref_pvecs.size(); ++i ) {
+            size_t len;
+            is >> len;
+            while( isspace( is.get() ) && !is.eof() ) {}
+            is.unget();
+            
+            if( is.eof() ) {
+                throw std::runtime_error( "unexpected end of file" );
+            }
+            
+            m_ref_pvecs[i].resize(len);
+            is.read( (char*)&m_ref_pvecs[i][0], len * sizeof(int) );
+            
+            m_ref_aux[i].resize(len);
+            is.read( (char*)&m_ref_aux[i][0], len * sizeof(unsigned int) );
+            
+//             std::cout << "ref: " << i << " " << m_ref_pvecs[i].size() << "\n";
+        }
+        
+    }
+    
+    
+public:  
+    testbench( const char *qs_name, const char *ref_name ) {
+        {
+            std::ifstream is( qs_name );
+            load_qs( is );
+        }
+        {
+            std::ifstream is( ref_name );
+            load_ref( is );
+        }
+        
+        
+        // reduce size of testset
+        size_t num_ref = 64;
+        size_t num_qs = 128;
+      
+        num_ref = std::min( num_ref, m_ref_pvecs.size() );
+        num_qs = std::min( num_qs, m_qs_pvecs.size() );
+        
+        m_ref_pvecs.resize(num_ref);
+        m_ref_aux.resize(num_ref);
+        m_qs_pvecs.resize(num_qs);
+        
+    }
+    
+    
+    void run_seq() {
+        align_arrays arr;
+        
+        uint64_t ncup = 0;
+        
+        timer t1;
+        
+        for( size_t i = 0; i < m_ref_pvecs.size(); ++i ) {
+            
+            const size_t reflen = m_ref_pvecs[i].size();
+            
+            for( size_t j = 0; j < m_qs_pvecs.size(); ++j ) {
+                
+
+                int score2 = 0;
+                int score = align_pvec_score_seq( m_ref_pvecs[i], m_ref_aux[i], m_qs_pvecs[j], score_mismatch, score_match_cgap, score_gap_open, score_gap_extend, arr );
+                //std::cout << "score " << i << " " << j << " = " << score << " " << score2 << "\n";
+//                 if( score != score2 ) {
+                    std::cout << "score " << i << " " << j << " = " << score << "\n";
+//                 }
+                ncup += reflen * m_qs_pvecs[i].size();
+                
+                m_seq_res[std::pair<size_t,size_t>(i,j)] = score;
+            }
+            
+            
+        }
+        
+        std::cerr << "aligned " << m_ref_pvecs.size() << " x " << m_qs_pvecs.size() << " seqs in " << t1.elapsed() << " s\n";
+        std::cerr << "GNcups: " << ncup / (t1.elapsed() * 1e9) << "\n";
+    }
+    
+    void run_vec() {
+        align_arrays arr;
+        
+        uint64_t ncup = 0;
+        
+        timer t1;
+        
+        const size_t W = 8;
+        typedef short score_t;
+        
+        for( size_t i = 0; i < m_ref_pvecs.size(); i += W ) {
+            const int *seqptrs[W];
+            const unsigned int *auxptrs[W];
+            size_t num_valid = 0;
+            for( size_t j = 0; j < W; ++j ) {
+                if( i + j < m_ref_pvecs.size() ) {
+                    seqptrs[j] = m_ref_pvecs[i+j].data();
+                    auxptrs[j] = m_ref_aux[i+j].data();
+                    ++num_valid;
+                } else {
+                    seqptrs[j] = seqptrs[0];
+                    auxptrs[j] = auxptrs[0];
+                }
+            }
+            
+            assert( num_valid > 0 );
+            
+            const size_t reflen = m_ref_pvecs[i].size();
+            
+            const align_pvec_score<score_t,W> aligner(seqptrs, auxptrs, reflen, score_mismatch, score_match_cgap, score_gap_open, score_gap_extend );
+            
+            
+//             std::cerr << "ref: " << i << "\n";
+            for( size_t j = 0; j < m_qs_pvecs.size(); ++j ) {
+                
+                aligner.align( m_qs_pvecs[j] );
+                const score_t *scores = aligner.get_scores();
+                
+                for( size_t k = 0; k < num_valid; ++k ) {
+                
+                    std::cout << "score " << i + k << " " << j << " = " << scores[k] << "\n";
+                    m_vec_res[std::pair<size_t,size_t>(i+k,j)] = scores[k];
+                }
+                ncup += num_valid * reflen * m_qs_pvecs[i].size();
+            }
+            
+            
+        }
+        
+        std::cerr << "aligned " << m_ref_pvecs.size() << " x " << m_qs_pvecs.size() << " seqs in " << t1.elapsed() << " s\n";
+        std::cerr << "GNcups: " << ncup / (t1.elapsed() * 1e9) << "\n";
+    }
+    
+    
+    void compare() {
+        
+        for( std::map< std::pair< size_t, size_t >, int >::iterator it = m_seq_res.begin(); it != m_seq_res.end(); ++it ) {
+            int seq_score = it->second;
+            int vec_score = m_vec_res[it->first];
+            
+            if( seq_score != vec_score ) {
+                std::cerr << "error: " << seq_score << " != " << vec_score << "\n";
+            }
+            
+        }
+    }
+};
+
+
+
+int main() {
+    testbench tb( "qs.bin", "ref.bin" );
+    tb.run_seq();
+    tb.run_vec();
+    
+    tb.compare();
+}
