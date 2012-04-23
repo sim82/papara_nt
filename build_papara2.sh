ae8c222e2afc29f369ca62d1d28703760c5cf05a
Simon Berger <bergers@in.tum.de>
2012-04-23 16:07:48 +0200

    removed pre-sssssss3 comiler problems of seriously ancient versions of gcc

--- build_papara2.sh
+++ build_papara2.sh
@@ -5,7 +5,7 @@
 
 
 
-g++ -o papara -O3 papara.cpp -mssse3 -I ivy_mike/src/ -I ublasJama-1.0.2.3 sequence_model.cpp parsimony.cpp ivy_mike/src/time.cpp ivy_mike/src/tree_parser.cpp ivy_mike/src/getopt.cpp ivy_mike/src/demangle.cpp ivy_mike/src/multiple_alignment.cpp ublasJama-1.0.2.3/EigenvalueDecomposition.cpp -lpthread  
+g++ -o papara -O3 papara.cpp -msse3 -I ivy_mike/src/ -I ublasJama-1.0.2.3 sequence_model.cpp parsimony.cpp ivy_mike/src/time.cpp ivy_mike/src/tree_parser.cpp ivy_mike/src/getopt.cpp ivy_mike/src/demangle.cpp ivy_mike/src/multiple_alignment.cpp ublasJama-1.0.2.3/EigenvalueDecomposition.cpp -lpthread  
 
 #-I/usr/include/boost141/
 
